3fa14dbc6b92acecc4a764eea70faa0e9779d167
PaulBucket33 <paul.kumar@mail.utoronto.ca>
2018-06-20 19:56:59 -0400

    committin changes

---
+++ HandySchedule/Assets/Scripts/Database/DatabaseHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class DatabaseHandler : MonoBehaviour {
+    DependencyStatus dS = DependencyStatus.UnavailableOther;
+
+
+    private DatabaseHandler()
+    {
+        //check dependencies -> for some reason task.result is undefined and shouldnt be
+        /*FirebaseApp.FixDependenciesAsync().ContinueWith(task =>
+        {
+            dS = task.Result;
+        });*/
+    }
+
+    private void InitializeFirebase()
+    {
+        FirebaseApp app = FirebaseApp.DefaultInstance;
+        app.SetEditorDatabaseUrl("NULL"); //need to define later
+        if(app.Options.DatabaseUrl != null)
+        {
+            app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
+        }
+        //StartListener();
+    }
+
+    protected void StartListener()
+    {
+        //DB update flow
+    }
+
+    protected void AddToDB(string eventName, 
+        string eventDay, string eventMonth, string eventYear, string eventTime)
+    {
+        //Adds the Event to the DB
+        Dictionary<string, object> newEvent = new Dictionary<string, object>();
+
+        //IMPORTANT. we need a consistent db addition/read flow. I think it should be
+        //as follows....
+
+        /*key={
+         * value1
+         * value2
+         * .
+         * .
+         * .
+         * .
+         * value_n} 
+         */
+         newEvent["Event Name"] = 
+    }
+
+
+    //handling the database
+}
---
+++ HandySchedule/Assets/Scripts/Drivers/CreateEventDriver.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateEventDriver : MonoBehaviour
+{
+    //public ui items
+    public Dropdown months;
+    public Dropdown days;
+
+    public InputField years;
+    public InputField eventName;
+
+    public Button createEventButton;
+    public Button clearFieldsButton;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        List<string> monthsList = new List<string>() { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+        /*months = GetComponent<Dropdown>();
+        days = GetComponent<Dropdown>();
+        years = GetComponent<InputField>();*/
+        //note when using get component you only use such when it is attached 
+        months.ClearOptions();
+        months.AddOptions(monthsList);
+
+        createEventButton.onClick.AddListener(OnCreate);
+        clearFieldsButton.onClick.AddListener(OnClear);
+
+        List<string> daysList = new List<string>() { };
+        for (int i = 0; i<=31; i++)
+        {
+            daysList.Add(i.ToString());
+        }
+        days.ClearOptions();
+        days.AddOptions(daysList);
+        
+    }
+
+    private void OnCreate()
+    {
+        //flow for adding the event to the db
+    }
+
+    private void OnClear()
+    {
+        years.text = "Year";
+        eventName.text = "Input Event Name";
+        days.RefreshShownValue();
+        months.RefreshShownValue();
+    }
+
+    void Update()
+    {
+
+    }
+
+
+}
