f60b090aef661b29cdfd8f9864a879c953409529
PaulBucket33 <paul.kumar@mail.utoronto.ca>
2018-06-27 23:23:29 -0400

    committing chnages

---
+++ HandySchedule/Assets/Scripts/Notifications/MessagingServices.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Messaging;
+using Firebase.Unity.Editor;
+using Firebase.Database;
+
+public class MessagingServices : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Firebase.Messaging.FirebaseMessaging.TokenReceived += 
+	}
+
+    public void OnTokenRecieved(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        Debug.Log("Recieved registration token: " + token.Token);
+    }
+
+    public void OnMessageRecieved(object sender, MessageReceivedEventArgs e)
+    {
+        Debug.Log("New message recieved from: " + e.Message.From);
+    }
+
+    private void CheckDependencies()
+    {
+        //checkdependenciesasync() doesnt exist for some reason
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
